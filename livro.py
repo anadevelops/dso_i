32fcf9b41d402724ad8bed8893dcb88cc6535fe1
Ana Clara <ana.stupp02@gmail.com>
2024-04-08 21:17:39 -0300

    .

---
+++ livro.py
@@ -0,0 +1,60 @@
+from editora import Editora
+from autor import Autor
+from capitulo import Capitulo
+
+class Livro:
+    def __init__(self, codigo: int, titulo: str, ano: int, editora: Editora, autor: Autor, numero_capitulo: int, titulo_capitulo: str):
+        self.__codigo = codigo
+        self.__titulo = titulo
+        self.__ano = ano
+        self.__editora = Editora(codigo, nome)
+        self.__autor = Autor(codigo, nome)
+        self.__numero_capitulo = 0
+        self.__titulo_capitulo = None
+        self.__autores = []
+        self.__capitulos = []
+
+    @property
+    def codigo(self):
+        return self.__codigo
+    
+    @codigo.setter
+    def codigo(self, codigo):
+        if isinstance(codigo, int):
+            self.__codigo = codigo
+
+    @property
+    def titulo(self):
+        return self.__titulo
+    
+    @titulo.setter
+    def titulo(self, titulo):
+        if isinstance(titulo, str):
+            self.__titulo = titulo
+
+    @property
+    def ano(self):
+        return self.__ano
+    
+    @ano.setter
+    def ano(self, ano):
+        if isinstance(ano, int):
+            self.__ano = ano   
+
+    @property
+    def editora(self):
+        return self.__editora
+    
+    @editora.setter
+    def editora(self, editora):
+        if isinstance(editora, Editora):
+            self.__editora = editora 
+
+    @property
+    def autores(self):
+        return self.__autores
+    
+    @editora.setter
+    def editora(self, editora):
+        if isinstance(editora, Editora):
+            self.__editora = editora                       
